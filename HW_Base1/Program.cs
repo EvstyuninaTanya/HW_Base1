e2b8ccfaaafb8c4f9bb8016b18ad712fa814dd74
Tatiana <lady.evstyunina@mail.ru>
2020-04-18 21:31:29 +0300

    Добавьте файлы проекта.

---
+++ HW_Base1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW_Base1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 0, 3, 11, 4, 5, 6, 13, 20, 44, 27 };
+
+            int n = 0;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] % 2 == 0)
+                {
+                    n += x[i];
+                }
+
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
